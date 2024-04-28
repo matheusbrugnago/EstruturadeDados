19475489b8d72d642bc6c591b0ad3f80c1311c00
Matheus Brugnago <106639493+matheusbrugnago@users.noreply.github.com>
2024-04-28 12:32:39 -0300

    Update Ordenacao.java

--- Exercicios/Ordenacao.java
+++ Exercicios/Ordenacao.java
@@ -11,6 +11,57 @@ public class Ordenacao {
             System.out.print(meuVetor[i]+" ");
         }
     }
+    public static void BubbleSort(int[] vetor){
+	    for( int i =v.lenght -1; i>0;i--){  // Ele passa de trás para frente
+		    for( int j=0; j < i ; j++){ // A comparação passa de frente para trás
+			    if (vetor[j] > vetor[j+1]){ //Se o número for maior  que o outro, deve-se trocar
+				    int temp = vetor[j]; 
+				    vetor[j] = vetor[j+1];
+				    vetor[j+1] = temp;
+		    }
+	    }
+    }
+    public static void BubbleSortV2(int[] vetor){
+	    for( int i =v.lenght -1; i>0;i--){
+		    boolean troca = false; 
+		for( int j=0; j < i ; j++){
+			if (vetor[j] > vetor[j+1]){
+				int temp = vetor[j];
+				vetor[j] = vetor[j+1];
+				vetor[j+1] = temp;
+				troca = true;
+				}
+		    }
+		if(!troca) return; // A validação é que se a variavel boolean não mudar, deve se terminar o método
+	    }
+    }
+    public static void BubbleSortRecursivo(int[] vetor ){
+			bubbleSortRecAux(vetor.length, vetor);  //método para puxar outro método(facilita para o programador)
+    }
+    private static void bubbleSortRecAux( int n, in[] vetor){
+			boolean troca = false;
+			for ( int j = 0; j< (n-1) ; j++){
+					if( vetor[j] > vetor[j+1]){
+						int temp = vetor[j];
+						vetor[j] = vetor[j+1]; //método de troca sendo feito
+						vetor[j+1] = temp;
+						troca = true;
+						}
+			}
+			if (troca) bubbleSortRecAux( n-1, vetor); //volta o método recursivo
+    }
+    public static void insertionSort( int[] vetor ){
+        int eleito, j;
+        for ( int i = 1; i< vetor.lenght; i++){
+        eleito = vetor[i];
+        j= i - 1;
+            while ( ( j>=0) && (eleito<vetor[j])) {
+              vetor[j+1] = vetor[j];
+              j--;
+            }
+        }
+        vetor[j+1] = eleito;
+    }
     public static void selectionSort(int[] vetor){
         for (int i = 0; i < (vetor.length-1); i++) {
             int MenorValor=vetor[i];
